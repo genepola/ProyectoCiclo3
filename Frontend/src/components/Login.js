17e870b4152b9e1aaf6f55868db6ad0b482c0d34
jhoanSV <jhoanjssv97@outlook.com>
2021-10-19 20:34:16 -0500

    intentos de loging

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -3,6 +3,10 @@ import Logo from '../assets/img/logo_col_tech.png'
 import SignIn from '../assets/img/Sign_In.png'
 import Background from '../assets/img/Fondo.png'
 
+const responseGoogle = (resp) =>{
+    console.log(resp);
+    
+}
 function Login() {
     return (
         <div>
@@ -14,6 +18,18 @@ function Login() {
                         <input class="controles" type="password" name="Contraseña" placeholder="Contraseña" />
                         <input class="botones" type="submit" name="" value="Ingresar" />
                         <img src={SignIn} alt="" id="SignIn" />
+                        
+                        
+
+                        <GoogleLogin
+                            classname = "Ingresar"
+                            clientId="513614895581-efcs9sriiinuo9v1ses66keutkctn7o5.apps.googleusercontent.com"
+                            buttonText="iniciar sesion con Google"
+                            onSuccess={responseGoogle}
+                            onFailure={responseGoogle}
+                            cookiePolicy={'single_host_origin'}
+                        />,
+                        
                     </section>
 
                 </body>
